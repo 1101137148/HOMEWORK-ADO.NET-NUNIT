73f0a95346f3249e02b1615ba49ddd97b2beb3bb
1101137148 <1101137148@gm.kuas.edu.tw>
2015-03-29 09:05:34 +0800

    add Nunit test

---
+++ Age/Calculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    public class Calculate
+    {
+        public int getAge(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = -1;
+            if (DateTime.Today >= birthday)
+            {
+                age = today.Year - birthday.Year;
+
+                if (today.Month < birthday.Month)
+                {
+                    age--;
+                }
+                else if (today.Month == birthday.Month)
+                {
+                    if (today.Day < birthday.Day)
+                    {
+                        age--;
+                    }
+                }
+            }
+            return age;
+        }
+    }
+}
---
+++ D20150327Homework.NunitTests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace D20150327Homework.NunitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CalAge()
+        {
+            Age.Calculate cal = new Age.Calculate();
+            
+            DateTime inputBirthday = Convert.ToDateTime("2015/03/29");
+            int expect = 0;
+
+            Assert.AreEqual(expect, cal.getAge(inputBirthday));
+        }
+    }
+}
